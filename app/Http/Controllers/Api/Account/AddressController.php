337766201cb2ada9974c019fef1f1fbd3c1d6d44
cainhooo <augustocaio663@gmail.com>
2025-08-07 19:33:09 -0300

    feat:Adicionou docs para Profile e Address no API Docs

--- app/Http/Controllers/Api/Account/AddressController.php
+++ app/Http/Controllers/Api/Account/AddressController.php
@@ -14,11 +14,41 @@ class AddressController extends Controller
 {
     public function __construct(protected $service = new AddressServices()) {}
 
+    /**
+     * @OA\Get(
+     *   path="/api/v1/me/address",
+     *   tags={"Endereços"},
+     *   summary="Informações sobre o endereço do usuário",
+     *   @OA\Response(
+     *     response=200,
+     *     description="OK",
+     *     @OA\JsonContent(
+     *       type="array",
+     *       @OA\Items(ref="#/components/schemas/AddressResponse")
+     *     )
+     *   )
+     * )
+     */
     public function index()
     {
         return AddressResource::collection(Auth::user()->addresses);
     }
-
+    /**
+     * @OA\Post(
+     *   path="/api/v1/me/address/create",
+     *   tags={"Endereços"},
+     *   summary="Criando um endereço padrão",
+     *   @OA\RequestBody(
+     *     required=true,
+     *     @OA\JsonContent(ref="#/components/schemas/AddressRequest")
+     *   ),
+     *   @OA\Response(
+     *     response=200,
+     *     description="OK",
+     *     @OA\JsonContent(ref="#/components/schemas/AddressResponse")
+     *   )
+     * )
+     */
     public function create(AddressRequest $request)
     {
         $data = $request->validated();
@@ -27,7 +57,22 @@ class AddressController extends Controller
 
         return new AddressResource($createdAddress);
     }
-
+    /**
+     * @OA\Post(
+     *   path="/api/v1/me/address",
+     *   tags={"Endereços"},
+     *   summary="Criando um endereço",
+     *   @OA\RequestBody(
+     *     required=true,
+     *     @OA\JsonContent(ref="#/components/schemas/AddressRequest")
+     *   ),
+     *   @OA\Response(
+     *     response=200,
+     *     description="OK",
+     *     @OA\JsonContent(ref="#/components/schemas/AddressResponse")
+     *   )
+     * )
+     */
     public function store(AddressRequest $request)
     {
         $data = $request->validated();
@@ -36,7 +81,28 @@ class AddressController extends Controller
 
         return new AddressResource($createdAddress);
     }
-
+    /**
+     * @OA\Patch(
+     *   path="/api/v1/me/address/{addressId}",
+     *   tags={"Endereços"},
+     *   summary="Atualizando um endereço",
+     *   @OA\Parameter(
+     *     name="addressId",
+     *     in="path",
+     *     required=true,
+     *     @OA\Schema(type="string")
+     *   ),
+     *   @OA\RequestBody(
+     *     required=true,
+     *     @OA\JsonContent(ref="#/components/schemas/AddressRequest")
+     *   ),
+     *   @OA\Response(
+     *     response=200,
+     *     description="OK",
+     *     @OA\JsonContent(ref="#/components/schemas/AddressResponse")
+     *   )
+     * )
+     */
     public function update(AddressRequest $request, int $address)
     {
         $data = $request->validated();
@@ -56,7 +122,31 @@ class AddressController extends Controller
             return response()->json(['error' => true, 'message' => $e->getMessage()], Response::HTTP_BAD_REQUEST);
         }
     }
-
+    /**
+     * @OA\Delete(
+     *   path="/api/v1/me/address/{addressId}",
+     *   tags={"Endereços"},
+     *   summary="Deletando um endereço",
+     *   @OA\Parameter(
+     *     name="addressId",
+     *     in="path",
+     *     required=true,
+     *     @OA\Schema(type="string")
+     *   ),
+     *   @OA\Response(
+     *     response=200,
+     *     description="OK",
+     *     @OA\JsonContent(
+     *       type="object",
+     *       @OA\Property(
+     *         property="message",
+     *         type="string",
+     *         description="Mensagem confirmando a exclusão do registro"
+     *       )
+     *     )
+     *   )
+     * )
+     */
     public function destroy(int $address)
     {
         $address = $this->service->getById($address);
