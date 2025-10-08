f840af8bf389efddb6a4eed750d086b88b35c281
cainhooow <augustocaio663@gmail.com>
2025-10-08 19:08:13 -0300

    Adicionou flags para login providers

--- app/Constants/Flags.php
+++ app/Constants/Flags.php
@@ -16,7 +16,6 @@ enum Flags
     case Can_Update_Services;
     case Can_Update_Users;
     case Can_Authenticate;
-
     case Local_Account_Provider;
     case Google_Account_Provider;
     case Facebook_Account_Provider;
